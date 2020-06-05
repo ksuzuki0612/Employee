1c44c43ec1090ad6133926aa3b53c00ab3d12f33
kaho <kaho.watanabe@ainmnext.com>
2020-06-05 12:30:28 +0900

    修正

--- UserMenu.java
+++ UserMenu.java
@@ -1,3 +1,9 @@
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.*;
 import java.text.*;
 import java.io.*;
