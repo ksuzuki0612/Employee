af6eed8ee533af4aec0a38f702357cbab472dfac
Kazutaka Hiramatsu <kazutaka.hiramatsu@aimnext.com>
2020-06-09 10:58:04 +0900

    Class名の修正 2020/06/09 10:57

--- checkOutBook.java
+++ checkOutBook.java
@@ -1,14 +1,6 @@
-import java.io.IOException;
-import java.util.function.Supplier;
-import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+
 //貸し出された本
-public class checkOutBook{
-	 Logger logger = Logger.getLogger(checkOutBook.class.getName());
+public class CheckOutBook{
     private int checkOutNumber;
     private long ISBN;
     private String title;
@@ -18,7 +10,6 @@ public class checkOutBook{
     private Date borrowedUntil;
 
     public checkOutBook(int checkOutNumber,long ISBN,String title,int employeeID,String employeeName,Date borrowedFrom,Date borrowedUntil){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
         this.checkOutNumber = checkOutNumber;
         this.ISBN = ISBN;
         this.title = title;
@@ -26,55 +17,38 @@ public class checkOutBook{
         this.employeeName = employeeName;
         this.borrowedFrom = borrowedFrom;
         this.borrowedUntil = borrowedUntil;
-        logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
     }
 
     public int getCheckOutNumber(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-    	logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.checkOutNumber;
     }
 
     public long getISBN(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-    	logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.ISBN;
     }
 
     public String getTitle(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-    	logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.title;
     }
 
     public int getEmployeeID(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-    	logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.employeeID;
     }
 
     public String getEmployeeName(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-    	logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.employeeName;
     }
 
     public Date getBorrowedFrom(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-    	logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.borrowedFrom;
     }
 
     public String getBorrowedUntil(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-    	logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.borrowedUntil;
     }
 
     public void setBorrowedUntil(String newBorrowedUntil){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
         this.borrowedUntil = newBorrowedUntil;
-        logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
     }
 
 }
--- reservedBook.java
+++ reservedBook.java
@@ -1,50 +1,33 @@
-import java.io.IOException;
-import java.util.function.Supplier;
-import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 //予約された本
-public class reservedBook{
-	Logger logger = Logger.getLogger(reservedBook.class.getName());
+public class ReservedBook{
     private int resevationNumber;
     private long ISBN;
     private String title;
     private String employeeName;
     private int resevationOrder;
-    public reservedBook(int resevationNumber,long ISBN,String title,String employeeName,int resevationOrder){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
+
+    public reservedBook(int resevationNumber, long ISBN, String title, String employeeName, int resevationOrder) {
         this.resevationNumber = resevationNumber;
         this.ISBN = ISBN;
         this.title = title;
         this.employeeName = employeeName;
         this.resevationOrder = resevationOrder;
-        logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
+
     }
 
     public int  getResevationNumber(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-        logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.resevationNumber;
     }
 
     public long getISBN(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-        logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.ISBN;
     }
 
     public String getTitle(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-        logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.title;
     }
 
     public int getResavationOrder(){
-    	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
-        logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
         return this.resevationOrder;
     }
 
