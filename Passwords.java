522f136bb62f25d55ce2cdef3832910999f26e96
ksuzuki0612 <kaisei.suzuki@aimnext.com>
2020-06-09 14:29:02 +0900

    log削除

--- Passwords.java
+++ Passwords.java
@@ -1,22 +1,15 @@
-import java.io.IOException;
-import java.util.function.Supplier;
-import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+
 
 public class Passwords{
-	  Logger logger = Logger.getLogger(Passwords.class.getName());
+	 
     private int employeeID;
     private String password;
 
     public  Passwords(int employeeID, String password) {
-    	  logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
+    	 
         this.employeeID = employeeID;
         this.password = password;
-        logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
+    
     }
 
     public int getEmployeeID(){
