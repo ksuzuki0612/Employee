8ddafa58abd8c722390906ab713875200a2b57e0
Kazutaka Hiramatsu <kazutaka.hiramatsu@aimnext.com>
2020-06-12 15:07:08 +0900

    再ログイン時に，プログラムが終了するエラーの修正

--- Login.java
+++ Login.java
@@ -39,6 +39,7 @@ public class Login {
      */
     public int loginCheck() throws SQLException {
     	logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
+    	
         try{
             final int empID = uiLogin.getEmpID();
             final String password = uiLogin.getPassword();
